545455a7b3df695f0f111f74934f728db746ff32
marugervasoni <gervasonimaria15@gmail.com>
2023-12-29 14:27:25 -0300

    c2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello 🌎 of React</h1>
+      <div className="card">
+        Here begins our adventure in React 😎😁
+      </div>
+    </>
+  )
+}
+
+export default App
